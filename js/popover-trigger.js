bf31ebd00953bac0b66a47c9c3abb2c3d8bffa58
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2014-04-21 10:22:36 -0300

    Added descriptions to popovers

--- js/popover-trigger.js
+++ js/popover-trigger.js
@@ -1,8 +1,8 @@
 function trigger_icons_popover(){
-	$('.icon1').popover({content: "We love small, specialpurpose computers, and we know how to get the most of them writing blazing fast software. We can also find the best way to get these devices to talk to the Cloud and between themselves. Meeting real ime constraints while controlling a variety of hardware peripherals is one of the things we do best. We can help you choose the right programming languages, oparating systems and hardware architecture to arrive to an optimal solution.",trigger: 'hover',container: 'body'});
+	$('.icon1').popover({content: "We love small, special purpose computers, and we know how to get the most of them writing blazing fast software.",trigger: 'hover',container: 'body'});
 	$('.icon2').popover({content: "Electronic board design is both art and science.",trigger: 'hover',container: 'body'});
-	$('.icon3').popover({content: "These days, in order for a tech business to be successful, it has to go mobile.",trigger: 'hover',container: 'body'});
+	$('.icon3').popover({content: "These days, in order for a tech business to be successful, it has to go mobile. We'll get you there.",trigger: 'hover',container: 'body'});
 	$('.icon4').popover({content: "The Web is where all we do gets connected.",trigger: 'hover',container: 'body'});
-	$('.icon5').popover({content: "In a world where signals and data are over-abundant...",trigger: 'hover',container: 'body'});
-	$('.icon6').popover({content: "",trigger: 'hover',container: 'body'});
+	$('.icon5').popover({content: "In a world where signals are over-abundant...",trigger: 'hover',container: 'body'});
+	$('.icon6').popover({content: "Data. Small or big. We know how to store it, model its relationships and get valuable information from it.",trigger: 'hover',container: 'body'});
 }
