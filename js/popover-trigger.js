cdd5b8d042a4086779f51875b0f919cfca510d9d
jvelazqueztraut <j.velazqueztraut@gmail.com>
2014-11-12 11:53:37 -0300

    Hover texts.

--- js/popover-trigger.js
+++ js/popover-trigger.js
@@ -1,8 +1,8 @@
 function trigger_icons_popover(){
 	$('.icon1').popover({content: "We love small, special purpose computers, and we know how to get the most of them writing blazing fast software.",trigger: 'hover',container: 'body'});
 	$('.icon2').popover({content: "Electronic board design is both art and science.",trigger: 'hover',container: 'body'});
-	$('.icon3').popover({content: "These days, in order for a tech business to be successful, it has to go mobile. We'll get you there.",trigger: 'hover',container: 'body'});
+	$('.icon3').popover({content: "If you want to succeed, your business has to go mobile. We'll get you there.",trigger: 'hover',container: 'body'});
 	$('.icon4').popover({content: "The Web is where all we do gets connected.",trigger: 'hover',container: 'body'});
-	$('.icon5').popover({content: "In a world where signals are over-abundant...",trigger: 'hover',container: 'body'});
+	$('.icon5').popover({content: "In this world signals are over-abundant; grasp them all.",trigger: 'hover',container: 'body'});
 	$('.icon6').popover({content: "Data. Small or big. We know how to store it, model its relationships and get valuable information from it.",trigger: 'hover',container: 'body'});
 }
