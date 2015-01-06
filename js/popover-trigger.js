1c72b719e98e554378d906e761df5cc705b742a5
Delfina <delfinapolito@gmail.com>
2015-01-06 18:16:44 -0300

    Using our development delorean to start all over again

--- js/popover-trigger.js
+++
@@ -1,8 +0,0 @@
-function trigger_icons_popover(){
-	$('.icon1').popover({content: "We love small, special purpose computers, and we know how to get the most of them writing blazing fast software.",trigger: 'hover' , placement: 'top auto',  container: 'body'});
-	$('.icon2').popover({content: "Electronic board design is both art and science.",trigger: 'hover' , placement: 'top auto', container: 'body'});
-	$('.icon3').popover({content: "If you want to succeed, your business has to go mobile. We'll get you there.",trigger: 'hover' , placement: 'top auto', container: 'body'});
-	$('.icon4').popover({content: "The Web is where all we do gets connected.",trigger: 'hover' , placement: 'top auto', container: 'body'});
-	$('.icon5').popover({content: "In this world signals are over-abundant; grasp them all.",trigger: 'hover' , placement: 'top auto', container: 'body'});
-	$('.icon6').popover({content: "Data. Small or big. We know how to store it, model its relationships and get valuable information from it.",trigger: 'hover' , placement: 'top auto', container: 'body'});
-}
