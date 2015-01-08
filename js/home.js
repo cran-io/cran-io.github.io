52e65b1666b100bd6a179d0499733b5565d66c84
Delfina <delfinapolito@gmail.com>
2015-01-08 18:56:01 -0300

    more sections of the web site

---
+++ js/home.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  if(!( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ) {
+ 	$("#title").addClass("superSize");
+    }
+
+  if(( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ) {
+ 	$('.miniOnMobile').addClass("miniSize"); 
+ 	}
+ 	// generate_gmap(-34.5854628, -58.4290632,"#googleMap");
+ 	function generate_gmap(lat,lng,srid) {
+		 var map;
+		 var coordinates = new google.maps.LatLng(-34.5854628, -58.4290632);
+		 var mapOptions = {
+		   zoom: 17,
+		   center: coordinates,
+		   scrollwheel: false
+		 }
+		 map = new google.maps.Map($("mapContainer"), mapOptions);
+		 var marker = new google.maps.Marker({
+		     map: map,
+		     position: coordinates
+		 });
+	}
+
+});
+
+
+// function generate_gmap(lat,lng,srid) {
+//  var map;
+//  var coordinates = new google.maps.LatLng(lat,lng);
+//  var mapOptions = {
+//    zoom: 17,
+//    center: coordinates,
+//    scrollwheel: false
+//  }
+//  map = new google.maps.Map(document.getElementById(srid), mapOptions);
+//  var marker = new google.maps.Marker({
+//      map: map,
+//      position: coordinates
+//  });
+// }
