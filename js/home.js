82702393d10cf268253478338c1333ef6066a12d
Delfina <delfinapolito@gmail.com>
2015-01-15 18:30:27 -0300

    center we are hiring

--- js/home.js
+++ js/home.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
 	var flag = true;
 	$(window).scroll(function(){
 		if(flag && $(window).scrollTop()>=$('#whatWeDo').offset().top && $(window).scrollTop()<$('#whatWeDo').offset().top+$('#whatWeDo').height()){
@@ -15,9 +14,9 @@ $(document).ready(function(){
 					$('.hardwareDefinitionDiv,.softwareDefinitionDiv').fadeOut(800,function(){
 						$('#hardwareIcons').fadeIn(1500);
 						$('#softwareIcons').fadeIn(1500);
-
 						$('#hardwareIcons').removeClass( "invisible" );
 						$('#softwareIcons').removeClass( "invisible" );
+
 
 
 					});
@@ -25,5 +24,59 @@ $(document).ready(function(){
 		    });
 		}
 	});
+	
+	$( ".workDiv" ).mouseenter( function(){
+		$(this).find(".logo").hide();
+		$(this).find(".informacion").show();
+		
+	});
+
+	$( ".workDiv" ).mouseleave( function(){
+		$(this).find(".informacion").hide();
+		$(this).find(".logo").show();
+	});
+	// $(window).on('resize', function(){
+
+		$('#weAreHiringButton').css({
+		'margin-top': -(Math.floor($('#weAreHiringButton').outerHeight(true)*0.5)),
+		'margin-left': -(Math.floor($('#weAreHiringButton').outerWidth(true)*0.5))
+		});
+		// $('.haveAnIdeaContent').css({
+		// 'margin-top': -(Math.floor($('.haveAnIdeaContent').outerHeight(true)*0.5)),
+		// 'margin-left': -(Math.floor($('.haveAnIdeaContent').outerWidth(true)*0.5))
+		// });
+
+	// });
+	
+
 });
+
+
+$(window).bind('orientationchange resize', function(event){
+ if (event.orientation) {
+   if (event.orientation == 'landscape') {
+     if (window.rotation == 90) {
+       rotate(this, -90);
+     } else {
+       rotate(this, 90);
+     }
+   }
+ }
+});
+
+function rotate(el, degs) {
+ iedegs = degs/90;
+ if (iedegs < 0) iedegs += 4;
+ transform = 'rotate('+degs+'deg)';
+ iefilter = 'progid:DXImageTransform.Microsoft.BasicImage(rotation='+iedegs+')';
+ styles = {
+   transform: transform,
+   '-webkit-transform': transform,
+   '-moz-transform': transform,
+   '-o-transform': transform,
+   filter: iefilter,
+   '-ms-filter': iefilter
+ };
+ $(el).css(styles);
+}
 
