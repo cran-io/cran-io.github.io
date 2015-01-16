494a7344c9e4e81f6daeb86408a49169f84067c6
Delfina <delfinapolito@gmail.com>
2015-01-16 17:31:06 -0300

    translations

---
+++ js/translation.js
@@ -0,0 +1,95 @@
+var translations = {}
+
+translations.es = {
+   slogan: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+
+   what_we_do: "QUE HACEMOS",
+   what_we_do_slogan: "Integramos hardware, firmware y software, para brindar soluciones tecnológicas a medida.",
+   hardware_content: "",
+   software_content: "",
+
+   how_we_do_it: "COMO LO HACEMOS",
+   how_we_do_it_slogan: "",
+   mobile_apps: "",
+   web_services: "",
+   data_processing: "",
+   hardware_design: "",
+   embbebed_systems: "",
+   signal_processing: "",
+
+
+   mobile_technology: "TECNOLOGIA",
+   mobile_technology_slogan: "",   
+   mobile_ mobile_apps_content: "",
+   mobile_ web_services_content: "",
+   mobile_ data_processing_content: "",
+   mobile_ hardware_design_content: "",
+   mobile_ embbebed_systems_content: "",
+   mobile_ signal_processing_content: "",
+
+   the_team: "EL EQUIPO",
+   the_team_slogan: "",
+
+   we_are_hiring_button: "",
+
+   last_works: "",
+   last_works_slogan: "", 
+
+   have_an_idea:"",
+   have_an_idea_first_parigraph:"",
+   have_an_idea_second_parigraph:"",
+   mail_us_button:"",
+
+   location: "",
+
+   modal_write_us: "",
+   modal_send: ""
+};
+
+translations.en = {
+    slogan: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+
+   what_we_do: "WHAT WE DO",
+   what_we_do_slogan: "",
+   hardware_content: "",
+   software_content: "",
+
+   how_we_do_it: "",
+   how_we_do_it_slogan: "",
+   mobile_apps: "",
+   web_services: "",
+   data_processing: "",
+   hardware_design: "",
+   embbebed_systems: "",
+   signal_processing: "",
+
+   mobile_technology: "",
+   mobile_technology_slogan: "",   
+   mobile_ mobile_apps_content: "",
+   mobile_ web_services_content: "",
+   mobile_ data_processing_content: "",
+   mobile_ hardware_design_content: "",
+   mobile_ embbebed_systems_content: "",
+   mobile_ signal_processing_content: "",
+
+   the_team: "THE TEAM",
+   the_team_slogan: "",
+
+   we_are_hiring_button: "",
+
+   last_works: "SOME WORKS",
+   last_works_slogan: "", 
+
+   have_an_idea:"",
+   have_an_idea_first_parigraph:"",
+   have_an_idea_second_parigraph:"",
+   mail_us_button:"",
+
+   location: "",
+
+   modal_write_us: "",
+   modal_send: ""
+};
+
+
+               
