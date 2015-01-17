3652b48ea94550850f4e95e07aaa6f8f31c75d6e
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2015-01-16 22:41:23 -0300

    Translations

--- js/translation.js
+++ js/translation.js
@@ -1,95 +1,91 @@
 var translations = {}
 
 translations.es = {
-   slogan: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+   slogan: "slogan en español",
 
    what_we_do: "QUE HACEMOS",
    what_we_do_slogan: "Integramos hardware, firmware y software, para brindar soluciones tecnológicas a medida.",
-   hardware_content: "",
-   software_content: "",
+   hardware_content: "La posibilidad de crear algo físico y tangible que sea un poderoso soporte electrónico para nuevas tecnologías.",
+   software_content: "La inteligencia de los dispositivos tecnológicos se escribe en código y nosotros manejamos los lenguajes para poder crear esa inteligencia.",
 
    how_we_do_it: "COMO LO HACEMOS",
-   how_we_do_it_slogan: "",
-   mobile_apps: "",
-   web_services: "",
-   data_processing: "",
-   hardware_design: "",
-   embbebed_systems: "",
-   signal_processing: "",
+   how_we_do_it_slogan: "how we do it slogan esp",
+   mobile_apps: "APLICACIONES MÓVILES",
+   web_services: "SERVICIO WEB",
+   data_processing: "PROCESAMIENTO DE DATOS",
+   hardware_design: "DISEÑO DE HARDWARE",
+   embedded_systems: "SISTEMAS EMBEBIDOS",
+   signal_processing: "PROCESAMIENTO DE SEÑALES",
 
-
-   mobile_technology: "TECNOLOGIA",
-   mobile_technology_slogan: "",   
-   mobile_ mobile_apps_content: "",
-   mobile_ web_services_content: "",
-   mobile_ data_processing_content: "",
-   mobile_ hardware_design_content: "",
-   mobile_ embbebed_systems_content: "",
-   mobile_ signal_processing_content: "",
+   technology: "TECNOLOGÍA",
+   technology_description: "descripcion tecnologia",   
+   mobile_apps_description: "Para ser exitoso, su negocio tiene que llegar a las plataformas móviles. Nosotros lo llevaremos.",
+   web_services_description: "La nube es conectividad junto a un increíble poder de procesamiento y almacenamiento.",
+   data_processing_description: "Sabemos cómo almacenar y modelar relaciones para obtener información valiosa de grandes bases de datos.",
+   hardware_design_description: "El diseño de placas electrónicas no sólo es ciencia. Es arte.",
+   embedded_systems_description: "Somos amantes de las micro computadoras tanto para prototipado rapido como para el desarrollo de productos.",
+   signal_processing_description: "Las señales abundan y nos rodean. La creación de sistemas que las utilicen no tienen límites.",
 
    the_team: "EL EQUIPO",
-   the_team_slogan: "",
+   the_team_description: "español",
 
-   we_are_hiring_button: "",
+   we_are_hiring_button: "español",
 
-   last_works: "",
-   last_works_slogan: "", 
+   latest_projects: "ULTIMOS PROYECTOS",
+   latest_projects_description: "español", 
 
-   have_an_idea:"",
-   have_an_idea_first_parigraph:"",
-   have_an_idea_second_parigraph:"",
-   mail_us_button:"",
+   have_an_idea_title:"español",
+   have_an_idea_first_paragraph:"español",
+   have_an_idea_second_paragraph:"español",
+   mail_us_button:"ESCRIBINOS",
 
-   location: "",
+   location: "español",
 
-   modal_write_us: "",
-   modal_send: ""
+   modal_write_us: "ESCRIBINOS!",
+   modal_send: "ENVIAR"
 };
 
 translations.en = {
     slogan: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
 
    what_we_do: "WHAT WE DO",
-   what_we_do_slogan: "",
-   hardware_content: "",
-   software_content: "",
+   what_we_do_slogan: "what we do slogan",
+   hardware_content: "hardware description",
+   software_content: "software description",
 
-   how_we_do_it: "",
-   how_we_do_it_slogan: "",
-   mobile_apps: "",
-   web_services: "",
-   data_processing: "",
-   hardware_design: "",
-   embbebed_systems: "",
-   signal_processing: "",
+   how_we_do_it: "how we do it",
+   how_we_do_it_slogan: "how we do it slogan",
+   mobile_apps: "MOBILE APPS",
+   web_services: "WEB SERVICES",
+   data_processing: "DATA PROCESSING",
+   hardware_design: "HARDWARE DESIGN",
+   embedded_systems: "EMBEDDED SYSTEMS",
+   signal_processing: "SIGNAL PROCESSING",
 
-   mobile_technology: "",
-   mobile_technology_slogan: "",   
-   mobile_ mobile_apps_content: "",
-   mobile_ web_services_content: "",
-   mobile_ data_processing_content: "",
-   mobile_ hardware_design_content: "",
-   mobile_ embbebed_systems_content: "",
-   mobile_ signal_processing_content: "",
+   technology: "TECHNOLOGY",
+   technology_description: "INGLES",   
+   mobile_apps_description: "INGLES",
+   web_services_description: "INGLES",
+   data_processing_description: "INGLES",
+   hardware_design_description: "INGLES",
+   embedded_systems_description: "INGLES",
+   signal_processing_description: "INGLES",
 
    the_team: "THE TEAM",
-   the_team_slogan: "",
+   the_team_description: "INGLES",
 
-   we_are_hiring_button: "",
+   we_are_hiring_button: "WE ARE HIRING",
 
-   last_works: "SOME WORKS",
-   last_works_slogan: "", 
+   latest_projects: "LATEST PROJECTS",
+   latest_projects_description: "INGLES", 
 
-   have_an_idea:"",
-   have_an_idea_first_parigraph:"",
-   have_an_idea_second_parigraph:"",
-   mail_us_button:"",
+   have_an_idea_title:"HAVE AN IDEA?",
+   have_an_idea_first_paragraph:"Come and sit with us in the table, we have cookies, coffee and lots of ideas too.",
+   have_an_idea_second_paragraph:"We can help your idea come true",
+   mail_us_button:"MAIL US",
 
-   location: "",
+   location: "LOCATION",
 
-   modal_write_us: "",
-   modal_send: ""
+   modal_write_us: "MAI US!",
+   modal_send: "SEND"
 };
-
-
-               
---
+++ js/translation_management.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+   set_texts("en");
+   $('.flag').click(function(){
+      set_texts($(this).data("language"));
+   });
+});
+
+function set_texts(language){
+   var t = translations[language];
+   // index
+   $('#slogan').html(t.slogan);
+   $('.whatWeDoTitle').each(function(){
+      $(this).html(t.what_we_do);
+   });
+   $('.whatWeDoSubtitle').each(function(){
+      $(this).html(t.what_we_do_slogan);
+   });
+
+   $('.hardwareDescription').each(function(){
+      $(this).html(t.hardware_content);
+   });
+   $('.softwareDescription').each(function(){
+      $(this).html(t.software_content);
+   });
+   
+
+   $('.webServices').each(function(){
+      $(this).html(t.web_services);
+   });
+   $('.dataProcessing').each(function(){
+      $(this).html(t.data_processing);
+   });
+   $('.mobileApps').each(function(){
+      $(this).html(t.mobile_apps);
+   });
+   $('.hardwareDesign').html(t.hardware_design);
+   $('.embeddedSystems').each(function(){
+      $(this).html(t.embedded_systems);
+   });
+   $('.signalProcessing').each(function(){
+      $(this).html(t.signal_processing);
+   });
+   
+   $('.technology').each(function(){
+      $(this).html(t.technology);
+   });
+   $('.technologyDescription').each(function(){
+      $(this).html(t.technology_description);
+   });
+   
+   $('.webServicesDescription').each(function(){
+      $(this).html(t.web_services_description);
+   });
+   $('.dataProcessingDescription').each(function(){
+      $(this).html(t.data_processing_description);
+   });
+   $('.mobileAppsDescription').each(function(){
+      $(this).html(t.mobile_apps_description);
+   });
+   $('.hardwareDesignDescription').each(function(){
+      $(this).html(t.hardware_design_description);
+   });
+   $('.embeddedSystemsDescription').each(function(){
+      $(this).html(t.embedded_systems_description);
+   });
+   $('.signalProcessingDescription').each(function(){
+      $(this).html(t.signal_processing_description);
+   });
+   $('.technologyDescription').each(function(){
+      $(this).html(t.technology_description);
+   });
+
+   $('.theTeam').html(t.the_team);
+   $('.theTeamDescription').html(t.the_team_description);
+
+   $("#weAreHiringButton").html(t.we_are_hiring_button);
+
+   $("#latestProjects").html(t.latest_projects);
+   $("#latestProjectsDescription").html(t.latest_projects_description);
+
+   $('.haveAnIdeaTitle').html(t.have_an_idea_title);
+   $('.haveAnIdeaFP').html(t.have_an_idea_first_paragraph);
+   $('.haveAnIdeaSP').html(t.have_an_idea_second_paragraph);
+   $('#mailUs').html(t.mail_us_button);
+
+   $('.locationTitle').html(t.location);
+
+   $('#myModalLabel').html(t.modal_write_us);
+   $('.sendCloseButtons').html(t.modal_send);
+}
