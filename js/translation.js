2b56b3117f8c35f9fd559753373a5b68927d1595
Javier Velazquez Traut <j.velazqueztraut@gmail.com>
2015-01-17 03:43:29 -0300

    Adding some text.

--- js/translation.js
+++ js/translation.js
@@ -1,24 +1,24 @@
 var translations = {}
 
 translations.es = {
-   slogan: "slogan en español",
+   slogan: "Desarrollo de Hardware y Software",
 
-   what_we_do: "QUE HACEMOS",
-   what_we_do_slogan: "Integramos hardware, firmware y software, para brindar soluciones tecnológicas a medida.",
+   what_we_do: "QUÉ HACEMOS",
+   what_we_do_slogan: "Somos un equipo multidisplinario que conoce las herramientas indicadas para crear nuevas tecnologías.",
    hardware_content: "La posibilidad de crear algo físico y tangible que sea un poderoso soporte electrónico para nuevas tecnologías.",
    software_content: "La inteligencia de los dispositivos tecnológicos se escribe en código y nosotros manejamos los lenguajes para poder crear esa inteligencia.",
 
-   how_we_do_it: "COMO LO HACEMOS",
-   how_we_do_it_slogan: "how we do it slogan esp",
+   how_we_do_it: "CÓMO LO HACEMOS",
+   how_we_do_it_slogan: "Integramos hardware, firmware y software, para brindar soluciones tecnológicas a medida.",
    mobile_apps: "APLICACIONES MÓVILES",
-   web_services: "SERVICIO WEB",
+   web_services: "SERVICIOS WEB",
    data_processing: "PROCESAMIENTO DE DATOS",
    hardware_design: "DISEÑO DE HARDWARE",
    embedded_systems: "SISTEMAS EMBEBIDOS",
    signal_processing: "PROCESAMIENTO DE SEÑALES",
 
    technology: "TECNOLOGÍA",
-   technology_description: "descripcion tecnologia",   
+   technology_description: "Tenemos la capacidad de innovar tanto en la selección de tecnologías como en su aplicación y la integración entre ellas.",   
    mobile_apps_description: "Para ser exitoso, su negocio tiene que llegar a las plataformas móviles. Nosotros lo llevaremos.",
    web_services_description: "La nube es conectividad junto a un increíble poder de procesamiento y almacenamiento.",
    data_processing_description: "Sabemos cómo almacenar y modelar relaciones para obtener información valiosa de grandes bases de datos.",
@@ -27,34 +27,34 @@ translations.es = {
    signal_processing_description: "Las señales abundan y nos rodean. La creación de sistemas que las utilicen no tienen límites.",
 
    the_team: "EL EQUIPO",
-   the_team_description: "español",
+   the_team_description: "Un equipo joven y audaz. Un equipo pensante y profesional.",
 
-   we_are_hiring_button: "español",
+   we_are_hiring_button: "TRABAJÁ CON NOSOTROS",
 
-   latest_projects: "ULTIMOS PROYECTOS",
+   latest_projects: "ÚLTIMOS PROYECTOS",
    latest_projects_description: "español", 
 
-   have_an_idea_title:"español",
-   have_an_idea_first_paragraph:"español",
-   have_an_idea_second_paragraph:"español",
+   have_an_idea_title:"¿TENÉS UNA IDEA?",
+   have_an_idea_first_paragraph:"Juntemosnos y charlemos en la mesa, tenemos café, galletitas y un montón de ideas también!",
+   have_an_idea_second_paragraph:"Podemos hacer que tu idea sea una realidad",
    mail_us_button:"ESCRIBINOS",
 
-   location: "español",
+   location: "DÓNDE ESTAMOS",
 
    modal_write_us: "ESCRIBINOS!",
    modal_send: "ENVIAR"
 };
 
 translations.en = {
-    slogan: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+   slogan: "Hardware and Software Development",
 
    what_we_do: "WHAT WE DO",
-   what_we_do_slogan: "what we do slogan",
-   hardware_content: "hardware description",
-   software_content: "software description",
+   what_we_do_slogan: "We are a multidisciplinary team with the right tools in hand.",
+   hardware_content: "We know how to design and create the tangible electronic support for new technologies.",
+   software_content: "Intelligent systems are written in code and we can handle the languages needed to bring life to that intelligence.",
 
-   how_we_do_it: "how we do it",
-   how_we_do_it_slogan: "how we do it slogan",
+   how_we_do_it: "HOW WE DO IT",
+   how_we_do_it_slogan: "We specialize in hardware, firmware and software integration to create new technological products.",
    mobile_apps: "MOBILE APPS",
    web_services: "WEB SERVICES",
    data_processing: "DATA PROCESSING",
@@ -64,28 +64,28 @@ translations.en = {
 
    technology: "TECHNOLOGY",
    technology_description: "INGLES",   
-   mobile_apps_description: "INGLES",
-   web_services_description: "INGLES",
-   data_processing_description: "INGLES",
-   hardware_design_description: "INGLES",
-   embedded_systems_description: "INGLES",
-   signal_processing_description: "INGLES",
+   mobile_apps_description: "If you want to succeed, your business has to go mobile. We'll get you there.",
+   web_services_description: "The Web is where all we do gets connected.",
+   data_processing_description: "Data. Small or big. We know how to store it, model its relationships and get valuable information from it.",
+   hardware_design_description: "Electronic board design is both art and science.",
+   embedded_systems_description: "We love small, special purpose computers, and we know how to get the most of them writing blazing fast software.",
+   signal_processing_description: "In this world signals are over-abundant; grasp them all.",
 
    the_team: "THE TEAM",
-   the_team_description: "INGLES",
+   the_team_description: "Young and courageous, yet thinkful and professional people. They will find passion in your project.",
 
    we_are_hiring_button: "WE ARE HIRING",
 
-   latest_projects: "LATEST PROJECTS",
-   latest_projects_description: "INGLES", 
+   latest_projects: "LAST PROJECTS",
+   latest_projects_description: "This is some of our portfolio", 
 
    have_an_idea_title:"HAVE AN IDEA?",
    have_an_idea_first_paragraph:"Come and sit with us in the table, we have cookies, coffee and lots of ideas too.",
-   have_an_idea_second_paragraph:"We can help your idea come true",
-   mail_us_button:"MAIL US",
+   have_an_idea_second_paragraph:"We can make your idea come true",
+   mail_us_button:"CONTACT US",
 
    location: "LOCATION",
 
-   modal_write_us: "MAI US!",
+   modal_write_us: "CONTACT US!",
    modal_send: "SEND"
 };
